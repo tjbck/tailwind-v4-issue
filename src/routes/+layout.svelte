c57e7e371baa4896f7288a2152ef4dc68329d5db
Timothy Jaeryang Baek <tim@openwebui.com>
2025-02-23 15:57:39 -0800

    i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,47 @@
-<script lang="ts">
+<script>
+    import { Toaster } from "svelte-sonner";
+
     import "../app.css";
     import "../tailwind.css";
+
+    import "tippy.js/dist/tippy.css";
+
+    import { setContext, onMount } from "svelte";
+    import i18n, { initI18n, getLanguages } from "$lib/i18n";
+
     let { children } = $props();
+
+    setContext("i18n", i18n);
+
+    onMount(async () => {
+        const languages = await getLanguages();
+
+        const browserLanguage = navigator.languages
+            ? navigator.languages[0]
+            : navigator.language || navigator.userLanguage;
+
+        initI18n("en-US");
+    });
 </script>
 
-{@render children()}
+<svelte:head>
+    <meta
+        name="keywords"
+        content="Open WebUI, Ollama, OpenAI, Chatbot, Local AI, Open-Source, Assistant Models, Language Models, Character AI"
+    />
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="language" content="English" />
+    <meta name="og:url" content="https://www.openwebui.com" />
+    <meta name="og:site_name" content="Open WebUI" />
+    <meta name="og:type" content="website" />
+</svelte:head>
+
+<Toaster position="top-center" richColors theme="dark" />
+
+<div
+    class="app h-fit min-h-screen bg-white text-gray-700 dark:bg-black dark:text-gray-100"
+>
+    {@render children()}
+</div>
